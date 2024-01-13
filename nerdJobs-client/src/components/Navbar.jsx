a0b756b89b75a05a68c8f9af5f812fc50a8eca99
Rim LAMNAOUAR <rimlamnaouar@gmail.com>
2024-01-14 00:05:17 +0100

    add some lignes in navbar

--- nerdJobs-client/src/components/Navbar.jsx
+++ nerdJobs-client/src/components/Navbar.jsx
@@ -1,24 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Navbar = () => {
-    const [isMenuOpen, setMenuOpen] = useState(false);
-    const handleMenuToggler = () =>{
-        setMenuOpen(!isMenuOpen)
-    }
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
-    const navItems = [
-        {path: '/', title: "start a search"},
-        {path: '/my-job', title: "My jobs"},
-        {path: 'salary', title: "Salary Estimate"},
-        {path: '/post-job', title: "Post a job"},
-    ]
+  const handleMenuToggler = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  const navItems = [
+    { path: "/", title: "start a search" },
+    { path: "/my-job", title: "My jobs" },
+    { path: "salary", title: "Salary Estimate" },
+    { path: "/post-job", title: "Post a job" },
+  ];
+
   return (
     <header>
-        <nav>
-            <a href="/">NerdJob</a>
-        </nav>
+      <nav>
+        <a href="/">NerdJob</a>
+        <ul>
+          {navItems.map((item, index) => (
+            <li key={index}>
+              <a href={item.path}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

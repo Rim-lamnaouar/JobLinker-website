0157033a2f36b03d7b61c1b2fd74609863b552e2
Rim LAMNAOUAR <rimlamnaouar@gmail.com>
2024-02-19 23:30:00 +0100

    Finalized navbar implementation; currently addressing banner functionality

--- JobLinker-client/src/components/Banner.jsx
+++ JobLinker-client/src/components/Banner.jsx
@@ -6,8 +6,17 @@ const Banner = () => {
         {/* <h1>Find your next adventure </h1>  */}
         <h1 className='text-5xl font-bold text-primary mb-3'>Find your <span style={{ color: '#00AEAD' }}>new job</span> in just a few clicks</h1>
         {/* <h2>Search for your dream job</h2> */}
-        <p className='text-lg text-black/70 mb-8'> </p>
-
+        <p className='text-lg text-black/70 mb-8'>
+          Search for your dream job and find the one that suits you the best. 
+          We have a wide range of job offers for you to choose from.
+        </p>
+        <form action="">
+          <div>
+            <div>
+              
+            </div>
+          </div>
+        </form>
         
     </div>
   )
